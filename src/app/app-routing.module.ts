97ec799b36046b82fb2c1c0202a70314909649d1
gesab001 <144000giovanni@gmail.com>
2021-07-14 01:53:13 +0000

    added bible and home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {AppComponent} from './app.component';
+import {BibleComponent} from './bible/bible.component';
+import {HomeComponent} from './home/home.component';
+const routes: Routes = [  
+     {path: '', component: HomeComponent},
+     {path: 'bible/:word', component: BibleComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { BibleComponent } from './bible/bible.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BibleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
